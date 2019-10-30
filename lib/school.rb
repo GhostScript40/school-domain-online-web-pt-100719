c27930bd43185fd5ee4ed8a9c0d514c5ef54ba6e
Tariq Bailey <tariqbailey627@gmail.com>
2019-10-30 01:38:37 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -15,10 +15,19 @@ class School
   
   def grade(grade)
     roster[grade]
+<<<<<<< HEAD
    end
   
   def sort
     roster[9].sort!
     roster.sort.to_h
+=======
+  end
+  
+  def sort
+    
+    roster[grade][name].sort
+    binding.pry
+>>>>>>> 45a3d562a1ec6ea30cb522eb957998ec7a05c286
   end
 end
